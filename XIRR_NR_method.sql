3001a51e29da730faa023e515f260e0b3ec92809
Bhaskara Hegde <65362333+bkhegde@users.noreply.github.com>
2024-04-08 22:46:32 +0530

    Initial

---
+++ XIRR_NR_method.sql
@@ -0,0 +1,95 @@
+
+CREATE OR REPLACE FUNCTION cox_schema.xirr_nr(
+	initial_guess numeric DEFAULT 0.1,
+	max_iterations integer DEFAULT 100,
+	debug_log boolean DEFAULT false)
+    RETURNS numeric
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+DECLARE
+	tolerance 		 DECIMAL(1,9) = 0.000000001;
+	iterator 		 INT = 0;
+	investment_count INT = 0;
+	income_count	 INT = 0;	
+	total_investment DECIMAL(40,8);
+	total_income	 DECIMAL(40,8);
+	f_xirr			 DECIMAL(40,8);
+	f_xirr_df		 DECIMAL(40,8);
+	xirr_n			 DECIMAL(20,8);
+	xirr_n1			 DECIMAL(20,8);
+
+BEGIN
+
+	-- Check for error conditions 
+	SELECT COUNT(*), SUM(transaction_value) INTO income_count, total_income
+	FROM  xirr_input
+	WHERE transaction_value > 0;
+
+	IF income_count = 0 THEN
+		RETURN -102;
+	END IF;
+
+	SELECT COUNT(*), SUM(-transaction_value) INTO investment_count, total_investment
+	FROM  xirr_input
+	WHERE transaction_value < 0;
+	
+	IF investment_count = 0 THEN
+		RETURN -103;
+	END IF;
+
+	-- pre-compute the date-diffs in years and fractions there of
+	UPDATE xirr_input 
+	SET delta_years = ( (SELECT MIN(transaction_date) FROM xirr_input) - transaction_date ) / 365.0;  -- Number of years lapsed (with -ve sign)
+	
+	-- Initial guess
+	xirr_n = initial_guess;
+	
+	WHILE iterator < max_iterations LOOP
+		
+		IF debug_log THEN
+			RAISE NOTICE 'XIRRn = %,  fx = %, dfx = %  ', xirr_n, f_xirr, f_xirr_df;
+		END IF;
+		
+		-- Compute the root and the derivative function values
+		BEGIN
+			SELECT 	SUM(transaction_value * (1 + xirr_n)^delta_years) ,  -- XIRR root function f(x)
+					SUM(delta_years * transaction_value * (1 + xirr_n)^(delta_years - 1.0)) -- derivative of XIRR root function f'(x)
+					INTO f_xirr, f_xirr_df
+			FROM xirr_input;
+		EXCEPTION -- Above computation may hit numerical limits and cause run-time errors
+			WHEN OTHERS THEN
+				RETURN -101; -- Errors like Numerical out of boud, division by zero etc. These occur when your xirr value is going out of bounds
+		END;
+		
+		--Newton-Raphson method says Xn+1 = Xn - f(x) / f'(x)
+		xirr_n1 = xirr_n - (f_xirr / f_xirr_df);  
+		
+		If ABS(xirr_n1 - xirr_n) < tolerance THEN -- we have reached the required precision
+			IF debug_log THEN
+				RAISE NOTICE 'XIRRn = %,  fx = %, dfx = %  ', xirr_n1, f_xirr, f_xirr_df;
+			END IF;
+			Return xirr_n1;
+		End If;
+		
+		-- Prepare the values for the next iteration
+		iterator = iterator + 1;
+		xirr_n = xirr_n1; -- This is standard Newton-Raphson menthod
+		
+		-- Below steps in not strictly necessary but is improvisation which may help at-times.
+		IF xirr_n1 > 100000 THEN -- 10,000,000% return is not a practical XIRR you encounter anyway and you are unlikely to converge
+			xirr_n = -0.99; -- reset the guess to the lowest possible value of -99% and see what happens
+		END IF;
+
+	END LOOP;
+
+	IF debug_log THEN
+		RAISE NOTICE 'No convergence even after % iterations ', iterator;
+	END IF;
+
+	Return -100; -- No convergence even after defined number of iterations
+
+END;
+$BODY$;
+
---
+++ XIRR_bisection_method.sql
@@ -0,0 +1,111 @@
+SET SEARCH_PATH = 'cox_schema';
+
+CREATE OR REPLACE FUNCTION xirr(debug_log boolean DEFAULT false)
+    RETURNS numeric
+    LANGUAGE 'plpgsql'
+
+AS $BODY$
+DECLARE
+	-- XIRR values starting at a wide interval and converge towards the true value
+	rmin	DECIMAL(12, 8) ;
+    rmax	DECIMAL(12, 8) ;
+    rmid	DECIMAL(12, 8) ;
+	rprev   DECIMAL(12, 8) ;
+
+	-- XNPV values as we converge towards the zero. These can go real big. Thus keep sufficient size.
+    fmin	DECIMAL(60, 8) ;
+    fmax 	DECIMAL(60, 8) ;
+	fmid	DECIMAL(60, 8) ;
+
+	iterator INT = 0;
+	t0 		 date;
+	investment_count 	INT =0;
+	income_count 		INT = 0;
+
+BEGIN
+
+	-- Check for error conditions 
+	SELECT COUNT(*) INTO investment_count
+	FROM xirr_inp
+	WHERE trns_val < 0;
+	
+	IF investment_count = 0 THEN
+		RETURN -101;
+	END IF;
+	
+	SELECT COUNT(*)  INTO income_count
+	FROM xirr_inp
+	WHERE trns_val > 0;
+
+	IF income_count = 0 THEN
+		RETURN -102;
+	END IF;
+
+	rmin = -0.99; 	-- can not go below -1 i.e. -100% profit XIRR formula fails at that value
+	rmax = 5;   	-- 500% profit
+	rprev = -1;  -- Random but outside the boundary value for the previous r value
+	
+	-- pre-compute the date-diffs
+	select min(trns_dt) into t0 from xirr_inp;
+	update xirr_inp set delta_t = (trns_dt - t0)/365.0;
+	
+	--Compute Vi values for the extreme outer range values
+	SELECT 	SUM(trns_val / (1 + rmin)^delta_t),
+			SUM(trns_val / (1 + rmax)^delta_t) INTO fmin, fmax
+	FROM xirr_inp;
+
+	WHILE iterator < 101 LOOP
+
+		rmid = (rmin + rmax)/2;
+		
+		IF debug_log THEN
+			RAISE NOTICE 'iteration# = % rmin = % rmid = % rmax = % rprev = % fmin = % fmid = % fmax = %', iterator, rmin, rmid, rmax, rprev, fmin, fmid, fmax;
+		END IF;
+		
+		--Compute Vi for the new rmid value
+		SELECT SUM(trns_val / (1 + rmid)^delta_t) INTO fmid
+		FROM xirr_inp;
+
+		If (ABS(fmid) < 0.0000001) OR (ABS(rmid-rprev) < 0.000001 ) THEN -- we have reached the required precision
+			IF debug_log THEN
+				RAISE NOTICE 'ABS(fmid) = %,  ABS(rmid-rprev)= %', ABS(fmid), ABS(rmid-rprev);
+			END IF;
+			
+			Return rmid;
+		End If;
+
+		iterator = iterator + 1;
+
+		-- Signs of Fmin and Fmax are same. can not converge in this scenario
+		If (fmin > 0 AND fmax > 0) OR (fmin <0 AND fmax < 0) Then
+			IF debug_log THEN
+				RAISE NOTICE 'XIRR Value is outside the limits given.';
+			END IF;
+			Return -103;
+		End If;
+
+		-- Signs of Fmin and Fmid are same	
+		If (fmin > 0 AND fmid > 0) OR (fmin < 0 AND fmid < 0) Then
+			rmin = rmid;
+			fmin = fmid;
+		Else -- Signs of fmax and fmid are same
+			rmax = rmid;
+			fmax = fmid;
+		ENd IF;
+		
+		rprev = rmid;
+		
+	END LOOP;
+
+	IF debug_log THEN
+		RAISE NOTICE 'Could not converge after 100 iterations. Last XIRRn = %', rmid;
+	END IF;
+
+	Return -100; -- No convergence even after 100 iterations
+
+EXCEPTION 
+	WHEN OTHERS THEN
+		RETURN -105; -- Numerical out of bound error occured
+END;
+$BODY$;
+
